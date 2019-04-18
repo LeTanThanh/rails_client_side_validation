276d9805bdee02a70091947fe5ca482606794ca1
LeTanThanh <tanthanhcr95@gmail.com>
2019-04-18 16:17:52 +0700

    add_user_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true,
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true,
+    length: { minimum: 6 },
+    confirmation: true
+  validates :password_confirmation, presence: true, on: :create
 end
