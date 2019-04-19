06d182df773c4c425686800a729fb1d0d7fb9261
LeTanThanh <tanthanhcr95@gmail.com>
2019-04-19 20:06:43 +0700

    add_users#new,#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,22 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = "User is created"
+      redirect_to @user
+    else
+      flash.now[:danger] = "User isn't created"
+      render :new
+    end
+  end
+
   def show; end
 
   def edit; end
@@ -32,6 +48,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit :name, :email
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
